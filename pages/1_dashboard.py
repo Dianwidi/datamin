de30158106c3eb72614cd438df891944e5c57da4
Dianwidi <dianwidianingrum270@students.unnes.ac.id>
2025-05-22 09:19:20 +0700

    Update 1_dashboard.py

--- pages/1_dashboard.py
+++ pages/1_dashboard.py
@@ -4,6 +4,6 @@ import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report,accuracy_score
 
-st.set_page_config(page_title"Irish Dashboard App",layot_"centered")
-st.tittle("Dashboard")
-st.header("Dashboard")
+st.set_page_config(page_title"Prediction")
+st.tittle("Prediction")
+st.header("Prediction")
